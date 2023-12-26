ac187aeea2d41620bc692d6bb46172e41f0b429f
SeemaGaikwadDeveloper <simagaikwad1234@gmail.com>
2023-12-26 17:04:02 +0530

    first commit

---
+++ ECommerce-20231221T133707Z-001/ECommerce/ECommerce/ECommerce.APITests/Controllers/ShoppingControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ECommerce.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace ECommerce.API.Controllers.Tests
+{
+    [TestClass()]
+    public class ShoppingControllerTests
+    {
+        [TestMethod()]
+        public void InsertPaymentTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
